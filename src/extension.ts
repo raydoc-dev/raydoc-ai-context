0f58e3275cf22b4eb1c2492eccbf627668059dee
Taylor Rowser <85259494+taylorrowser@users.noreply.github.com>
2025-02-09 19:04:49 -0700

    One command to rule them all (#6)
    * Add types to TypeDefinition
    
    * Add most fields to context
    
    * File tree fixes
    
    * Push errors over to new context storing
    
    * Remove some unnecessary imports
    
    * Remove reptiles file from launch.json
    
    * Consolidate commands

--- src/extension.ts
+++ src/extension.ts
@@ -34,11 +34,17 @@ export function activate(context: vscode.ExtensionContext) {
         async () => { copyLineContextAtCursorCommandHandler(); }
     );
 
+    const copyContextAtCursorCommand = vscode.commands.registerCommand(
+        'raydoc-context.copyContextAtCursor',
+        async () => { copyContextAtCursorCommandHandler(); }
+    );
+
     context.subscriptions.push(
         providerDisposable,
         copyErrorContextCommand,
         copyErrorContextAtCursorCommand,
         copyLineContextAtCursorCommand,
+        copyContextAtCursorCommand,
     );
 }
 
@@ -89,6 +95,40 @@ async function copyErrorContextAtCursorCommandHandler() {
         vscode.window.showWarningMessage('No error at the current cursor position.');
         return;
     }
+
+    const context = await gatherContext(doc, position, diag);
+
+    if (!context) {
+        vscode.window.showErrorMessage('No context found for the current cursor position.');
+        return;
+    }
+
+    const output = contextToString(context);
+    if (output) {
+        await vscode.env.clipboard.writeText(output);
+        vscode.window.showInformationMessage('Raydoc: context copied to clipboard!');
+    } else {
+        vscode.window.showWarningMessage('No context available to copy.');
+    }
+}
+
+async function copyContextAtCursorCommandHandler() {
+    // Attempt to detect an error at the current cursor position
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showWarningMessage('No active text editor.');
+        return;
+    }
+
+    const position = editor.selection.active; // Cursor position
+    const doc = editor.document;
+    const docUri = doc.uri;
+
+    // Get all diagnostics for this file
+    const diagnostics = vscode.languages.getDiagnostics(docUri);
+
+    // Find a diagnostic that covers the cursor position
+    const diag = diagnostics.find(d => d.range.contains(position));
 
     const context = await gatherContext(doc, position, diag);
 
